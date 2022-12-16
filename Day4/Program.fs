f6f93d509e1982d6d06b655d48ae41392ba64fc5
jamesjohnson <5540346+jamesjohnson88@users.noreply.github.com>
2022-12-16 23:01:30 +0000

    WIP Commit

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,12 +1,26 @@
 
+open System
 open System.IO
+
+///////////
+// Types //
+///////////
+type Assignment (lowerValue:string, upperValue:string) =
+    member this.LowerValue = Convert.ToInt16(lowerValue)
+    member this.UpperValue = Convert.ToInt16(upperValue)
 
 ///////////////
 // Functions //
 ///////////////
-
+let parseAsAssignment = fun(x:string) ->
+    Assignment("2", "5") //todo
+    
 let compareAssignments = fun(x:string) ->
-    "hello"
+    let split = x.Split(',')
+    let assignment1 = parseAsAssignment split[0]
+    let assignment2 = parseAsAssignment split[1]
+    true
+
 
 /////////////
 // Program //
